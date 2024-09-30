6bd11c7fbff4782c4d2ac1cb961da1accff7549b
TylerTankersley <tdt0022@uah.edu>
2024-09-30 14:31:27 -0500

    Changed Winform

---
+++ Desktop_App/G04_FAM_Desktop_App/Form1.cs
@@ -0,0 +1,15 @@
+namespace G04_FAM_Desktop_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
